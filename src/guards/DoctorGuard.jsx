9e27170466f45f83ff17e97fd2d4790763a3cd08
Ali Ahmad <aliahmadrizvi88@gmail.com>
2026-01-27 12:21:11 +0500

    deployment commit

--- src/guards/DoctorGuard.jsx
+++ src/guards/DoctorGuard.jsx
@@ -2,9 +2,10 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../context/Auth/DoctorAuth/useAuth';
 
 const DoctorGuard = () => {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, token, doctor } = useAuth();
 
-  if (!isAuthenticated) {
+  if (!isAuthenticated || !token || !doctor) {
+    sessionStorage.clear();
     return <Navigate to="/auth/doc-signIn" replace />;
   }
 
